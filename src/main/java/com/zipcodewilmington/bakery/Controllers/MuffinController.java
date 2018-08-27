6eae50f4e09558ea30551acf758573be99e9d94e
Dominique Clarke <dominiquedclarke@gmail.com>
2018-08-27 15:54:27 -0400

    updated pom.xml to earlierspring starter boot version, exposed refresh endpoint

--- src/main/java/com/zipcodewilmington/bakery/Controllers/MuffinController.java
+++ src/main/java/com/zipcodewilmington/bakery/Controllers/MuffinController.java
@@ -2,36 +2,44 @@ package com.zipcodewilmington.bakery.Controllers;
 
 import com.zipcodewilmington.bakery.Models.Muffin;
 import com.zipcodewilmington.bakery.Repositories.MuffinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MuffinController {
 
+    @Autowired
     private MuffinRepository muffinRepository;
 
+    @GetMapping("/muffins")
     public ResponseEntity<Iterable<Muffin>> index() {
         return new ResponseEntity<>(this.muffinRepository.findAll(), HttpStatus.OK);
     }
 
-    public ResponseEntity<Muffin> show(Long id) {
-        return new ResponseEntity<>(this.muffinRepository.findById(id).get(), HttpStatus.OK);
+    @GetMapping("/muffins/{id}")
+    public ResponseEntity<Muffin> show(@PathVariable Long id) {
+        return new ResponseEntity<>(this.muffinRepository.findOne(id), HttpStatus.OK);
     }
 
-    public ResponseEntity<Muffin> create(Muffin muffin) {
+    @PostMapping("/muffins")
+    public ResponseEntity<Muffin> create(@RequestBody Muffin muffin) {
         return new ResponseEntity<>(this.muffinRepository.save(muffin), HttpStatus.CREATED);
     }
 
-    public ResponseEntity<Muffin> update(Long id, Muffin muffin) {
-        Muffin foundMuffin = muffinRepository.findById(id).get();
+    @PutMapping("/muffins/{id}")
+    public ResponseEntity<Muffin> update(@PathVariable Long id, @RequestBody Muffin muffin) {
+        Muffin foundMuffin = muffinRepository.findOne(id);
         foundMuffin.setFlavor(muffin.getFlavor());
 
         return new ResponseEntity<>(this.muffinRepository.save(foundMuffin), HttpStatus.OK);
     }
 
-    public ResponseEntity<Boolean> destroy(Long id) {
-        this.muffinRepository.deleteById(id);
+    @DeleteMapping("/muffins/id")
+    public ResponseEntity<Boolean> destroy(@PathVariable Long id) {
+        this.muffinRepository.delete(id);
         return new ResponseEntity<>(true, HttpStatus.OK);
     }
 
